cd164045acf0332963655535fc29047973ed32b9
Zeki SERT <zeki.sert@yahoo.com>
2020-10-24 23:59:09 +0300

    ProductManager Eklendi

---
+++ Ecommerce.BLL/ProductManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using Ecommerce.BLL.Abstraction.Base;
+using Ecommerce.BLL.Abstraction.Contracts;
+using Ecommerce.Models.DTO;
+using Ecommerce.Models.EntityModels;
+using Ecommerce.Repository.Abstraction.Contracts;
+
+namespace Ecommerce.BLL
+{
+    public class ProductManager : Manager<Product>,IProductManager
+    {
+        private IProductRepository _productRepository;
+        public ProductManager(IProductRepository productRepository) : base(productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public bool Add(Product entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ICollection<Product> Search(ProductSearchCriteriaDTO dto)
+        {
+            return _productRepository.Search(dto);
+        }
+
+        public ICollection<Product> GetShopId(int shopId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
