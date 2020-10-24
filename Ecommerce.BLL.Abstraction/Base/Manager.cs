323b27d462d355cefc1ade860c56a646b11b561f
Zeki SERT <zeki.sert@yahoo.com>
2020-10-24 23:17:16 +0300

    - Ecommerce.BLL Abstraction Eklenmesi - Manager Class Eklenmesi

---
+++ Ecommerce.BLL.Abstraction/Base/Manager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using Ecommerce.Repository.Abstraction.Contracts;
+
+namespace Ecommerce.BLL.Abstraction.Base
+{
+    public abstract class Manager<T> where T : class
+    {
+        private IRepository<T> _repository;
+
+        protected Manager(IRepository<T> repository)
+        {
+            _repository = repository;
+        }
+
+        public virtual bool Update(T entity)
+        {
+            _repository.Update(entity);
+            return true;
+        }
+
+        public virtual bool Remove(T entity)
+        {
+            _repository.Remove(entity);
+            return true;
+        }
+
+        public virtual ICollection<T> GetAll()
+        {
+            return _repository.GetAll();
+        }
+
+        public virtual T GetById(int id)
+        {
+            return _repository.GetById(id);
+        }
+    }
+}
