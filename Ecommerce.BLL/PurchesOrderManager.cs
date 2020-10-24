7da26135d138d9b1a4fc3967bd3054d257a96d3e
Zeki SERT <zeki.sert@yahoo.com>
2020-10-25 00:01:35 +0300

    PurchesOrderManager Eklendi

---
+++ Ecommerce.BLL/PurchesOrderManager.cs
@@ -0,0 +1,21 @@
+﻿using Ecommerce.BLL.Abstraction.Base;
+using Ecommerce.BLL.Abstraction.Contracts;
+using Ecommerce.Models.EntityModels;
+using Ecommerce.Repository.Abstraction.Contracts;
+
+namespace Ecommerce.BLL
+{
+    public class PurchesOrderManager : Manager<PurchesOrder>,IPurchesOrderManager
+    {
+        private IPurchesOrderRepository _purchesorderRepository;
+        public PurchesOrderManager(IPurchesOrderRepository purchesOrderRepository) : base(purchesOrderRepository)
+        {
+            _purchesorderRepository = purchesOrderRepository;
+        }
+
+        public bool Add(PurchesOrder entity)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
