0b92dfcd8f2857b18d9afc240a2ee80b2b0f73a5
Zeki SERT <zeki.sert@yahoo.com>
2020-10-24 23:55:30 +0300

    CustomerManager Eklendi

---
+++ Ecommerce.BLL/CustomerManager.cs
@@ -0,0 +1,21 @@
+﻿using Ecommerce.BLL.Abstraction.Base;
+using Ecommerce.BLL.Abstraction.Contracts;
+using Ecommerce.Models.EntityModels;
+using Ecommerce.Repository.Abstraction.Contracts;
+
+namespace Ecommerce.BLL
+{
+    public class CustomerManager:Manager<Customer>,ICustomerManager
+    {
+        private ICustomerRepository _customerRepository;
+        public CustomerManager(ICustomerRepository customerRepository) : base(customerRepository)
+        {
+            _customerRepository = customerRepository;
+        }
+
+        public bool Add(Customer entity)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
