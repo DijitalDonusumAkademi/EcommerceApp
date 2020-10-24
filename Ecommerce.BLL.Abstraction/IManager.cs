100b9f3f86671944aa1873910289de5fc46911ae
Zeki SERT <zeki.sert@yahoo.com>
2020-10-24 23:38:54 +0300

    IManager Interface Eklendi

---
+++ Ecommerce.BLL.Abstraction/IManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Ecommerce.BLL.Abstraction
+{
+    public interface IManager<T> where T : class
+    {
+        bool Add(T entity);
+        bool Update(T entity);
+        bool Remove(T entity);
+        ICollection<T> GetAll();
+        T GetById(int id);
+    }
+}
