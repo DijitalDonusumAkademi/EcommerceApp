af2b41a23486e7045a061561a86173a9c570282c
Zeki SERT <zeki.sert@yahoo.com>
2020-10-25 00:03:39 +0300

    ShopManager Eklendi

---
+++ Ecommerce.BLL/ShopManager.cs
@@ -0,0 +1,25 @@
+﻿using Ecommerce.BLL.Abstraction.Base;
+using Ecommerce.Models.EntityModels;
+using Ecommerce.Repository.Abstraction.Contracts;
+
+namespace Ecommerce.BLL
+{
+    public class ShopManager : Manager<Shop>,IShopManager
+    {
+        private IShopRepository _shopRepository;
+        public ShopManager(IShopRepository shopRepository) : base(shopRepository)
+        {
+            _shopRepository = shopRepository;
+        }
+
+        public bool Add(Shop entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void LoadProduct(Shop shop)
+        {
+            _shopRepository.LoadProduct(shop);
+        }
+    }
+}
