915a182e076075d95e80eefe7aa0dbfe3cc6966c
Zeki SERT <zeki.sert@yahoo.com>
2020-10-24 23:42:38 +0300

    IProductManager Eklendi

---
+++ Ecommerce.BLL.Abstraction/IProductManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Ecommerce.Models.DTO;
+using Ecommerce.Models.EntityModels;
+
+namespace Ecommerce.BLL.Abstraction
+{
+    public interface IProductManager:IManager<Product>
+    {
+        ICollection<Product> Search(ProductSearchCriteriaDTO dto);
+        ICollection<Product> GetShopId(int shopId);
+    }
+}
